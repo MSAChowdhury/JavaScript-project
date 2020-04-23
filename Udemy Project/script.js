849716692962081a0d9ec7b851a9753b39d52c50
MSAChowdhury <samselarifinchowdhury@gmail.com>
2020-04-24 00:01:15 +0600

    JS Ex:1

---
+++ Udemy Project/script.js
@@ -0,0 +1,6 @@
+alert("Welcome to site");
+var weight = prompt("Enter the weight (LBS)");
+alert("You entered the wieghts in lbs: "+weight);
+var kilogram = weight * 0.454;
+alert("Converted weights: "+kilogram);
+console.log("Conversion Completed!");
